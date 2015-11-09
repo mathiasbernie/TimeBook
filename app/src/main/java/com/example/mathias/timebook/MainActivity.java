97ee51af8d16820c5a04259139e5f6d66f6805d8
niklasnachtweij <n.nachtweij@gmail.com>
2015-11-09 13:16:56 +0100

    Calculator beta 2.1

--- app/src/main/java/com/example/mathias/timebook/MainActivity.java
+++ app/src/main/java/com/example/mathias/timebook/MainActivity.java
@@ -1,59 +1,21 @@
 package com.example.mathias.timebook;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button calc_button;
-    EditText first_textview;
-    EditText second_textview;
-    TextView result_label;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.testlayout);
-
-
-        Initiate();
-    }
-
-    private void Initiate(){
-
-        calc_button = (Button) findViewById(R.id.calc_btn);
-        first_textview = (EditText) findViewById(R.id.indatatext1);
-        second_textview = (EditText) findViewById(R.id.indatatext2);
-        result_label = (TextView) findViewById(R.id.resultLabel);
-
-    }
-
-    private void Calculate(){
-
-        int firstvalue = Integer.parseInt(first_textview.getText().toString());
-        int secondvalue = Integer.parseInt(second_textview.getText().toString());
-        int sum = firstvalue + secondvalue;
-        result_label.setText(Integer.toString(sum));
+        setContentView(R.layout.activity_main);
 
 
     }
 
-
-    public void OnCalculateClick(View v) {
-
-        Calculate();
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
