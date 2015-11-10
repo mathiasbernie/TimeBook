1027eede68efb424086a1cf81f8cfddc2184a9a3
niklasnachtweij <n.nachtweij@gmail.com>
2015-11-10 12:20:08 +0100

    Fixat med DatabaseBookingHandler

--- app/src/main/java/com/example/mathias/timebook/Database/DatabaseBookingHandler.java
+++ app/src/main/java/com/example/mathias/timebook/Database/DatabaseBookingHandler.java
@@ -6,13 +6,8 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 import com.example.mathias.timebook.BookingModel.Booking;
 import com.example.mathias.timebook.BookingModel.Customer;
-
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 
